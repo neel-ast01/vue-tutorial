38b0f54d3c771becdaa27cf73b78198793c87787
neel002b <neelbhalodiya555@gmail.com>
2024-06-14 11:25:19 +0530

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,9 @@
 <script>
-import _ from "lodash";
-export default {
-  data() {
-    return {
-      names: ["Neel", "Vasu", "Harsh"],
-      movies: [
-        {
-          title: "Dilwale Dulhania Le Jayenge",
-        },
-        {
-          title: "Sholay",
-        },
-        {
-          title: "3 Idiots",
-        },
-        {
-          title: "Dangal",
-        },
-        {
-          title: "Kabhi Khushi Kabhie Gham",
-        },
-      ],
-    };
-  },
-  methods: {
-    shuffle() {
-      console.log("Button is Press");
-      this.names = _.shuffle(this.names);
-    },
-  },
-};
+export default {};
 </script>
 
 <template>
-  <div v-for="name in names" :key="name">
-    <h2>{{ name }}</h2>
-    <input type="text" placeholder="Enter your Last Name" />
-    <hr />
-  </div>
-  <button @click="shuffle">Shuffle</button>
-  <div>
-    <ul>
-      <li v-for="movie in movies" :key="movie.title">
-        {{ movie.title }}
-      </li>
-    </ul>
-  </div>
+  <div>Hello</div>
 </template>
 
 <style></style>
